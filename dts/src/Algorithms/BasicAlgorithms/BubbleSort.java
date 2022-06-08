bf6aace92f74c6b487b416607d105cb793689a14
mustafaguler3 <mustafa.glr@hotmail.com>
2022-06-08 17:21:07 +0300

    bubble sort algorithm added

---
+++ dts/src/Algorithms/BasicAlgorithms/BubbleSort.java
@@ -0,0 +1,14 @@
+package Algorithms.BasicAlgorithms;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array){
+        for (int i = array.length - 1; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
